52bcb0a28be57963591c30071cef8fe455b97c39
Mateus Sales <matheussalles40@gmail.com>
2025-08-02 14:08:36 -0300

    feat(owner): ajusta função main estava com erro de compilação

--- bin/dart_leetcode.dart
+++ bin/dart_leetcode.dart
@@ -1,6 +1,1 @@
-import 'package:dart_leetcode/is_palindrome/palindrome.dart';
-
-void main(List<String> arguments) {
-  final pal = Palindrome();
-  print(pal.isPalindrome(12221));
-}
+void main(List<String> arguments) { }
